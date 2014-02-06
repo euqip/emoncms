da33d0a1a450022c3acbb73d9bdf5457548d0c20
euqip <benoit.pique@gmail.com>
2014-02-06 18:56:57 +0100

    upgrade to jquery 1.11.0

--- Theme/theme.php
+++ Theme/theme.php
@@ -17,7 +17,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <script type="text/javascript" src="<?php echo $path; ?>Lib/jquery-1.9.0.min.js"></script>
+        <script type="text/javascript" src="<?php echo $path; ?>Lib/jquery-1.11.0.js"></script>
         <link href="<?php echo $path; ?>Lib/bootstrap/css/bootstrap.css" rel="stylesheet">
         <link href="<?php echo $path; ?>Lib//bootstrap-dialog/css/bootstrap-dialog.css" rel="stylesheet" type="text/css" />
         <script src="<?php echo $path; ?>Lib//bootstrap-dialog/js/bootstrap-dialog.js"></script>
