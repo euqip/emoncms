78d1e326d61789d90298c0095bc90e41411fd7b7
euqip <benoit.pique@gmail.com>
2014-05-08 22:52:03 +0200

    reindent table.je file

--- Lib/tablejs/custom-table-fields.js
+++ Lib/tablejs/custom-table-fields.js
@@ -5,7 +5,7 @@
 
   Part of the OpenEnergyMonitor project:
   http://openenergymonitor.org
- 
+
 */
 //custom table fields is not necessary when all types of fields are in table.js!
 
@@ -42,34 +42,28 @@
 
     'processlist':
     {
-        'draw': function (row,field) { 
-
+        'draw': function (row,field) {
           var processlist = table.data[row][field];
           if (!processlist) return "";
-          
           var processPairs = processlist.split(",");
-
           var out = "";
-
           for (z in processPairs)
           {
             var keyvalue = processPairs[z].split(":");
-
             var key = parseInt(keyvalue[0]);
             var type = "";
             var color = "";
-
             switch(key)
             {
               case 1:
                 key = 'log'; type = 2; break;
-              case 2:  
+              case 2:
                 key = 'x'; type = 0; break;
-              case 3:  
+              case 3:
                 key = '+'; type = 0; break;
-              case 4:    
+              case 4:
                 key = 'kwh'; type = 2; break;
-              case 5:  
+              case 5:
                 key = 'kwhd'; type = 2; break;
               case 6:
                 key = 'x inp'; type = 1; break;
@@ -79,9 +73,9 @@
                 key = 'kwhinckwhd'; type = 2; break;
               case 9:
                 key = 'kwhkwhd'; type = 2; break;
-              case 10:  
+              case 10:
                 key = 'update'; type = 2; break;
-              case 11: 
+              case 11:
                 key = '+ inp'; type = 1; break;
               case 12:
                 key = '/ inp'; type = 1; break;
@@ -93,7 +87,7 @@
                 key = 'rate'; type = 2; break;
               case 16:
                 key = 'hist'; type = 2; break;
-              case 17:  
+              case 17:
                 key = 'average'; type = 2; break;
               case 18:
                 key = 'flux'; type = 2; break;
@@ -127,10 +121,10 @@
                 key = '/ feed'; type = 4; break;
               case 33:
                 key = '= 0'; type = 3; break;
-            }  
+            }
 
             value = keyvalue[1];
-            
+
             switch(type)
             {
               case 0:
@@ -151,22 +145,22 @@
                 break;
             }
 
-            if (type == 'feed: ') { 
-              out += "<a href='"+path+"vis/auto?feedid="+value+"'<span class='label label-"+color+"' title='"+type+value+"' style='cursor:pointer'>"+key+"</span></a> "; 
+            if (type == 'feed: ') {
+              out += "<a href='"+path+"vis/auto?feedid="+value+"'<span class='label label-"+color+"' title='"+type+value+"' style='cursor:pointer'>"+key+"</span></a> ";
             } else {
               out += "<span class='label label-"+color+"' title='"+type+value+"' style='cursor:default'>"+key+"</span> ";
             }
           }
-          
+
           return out;
         }
     },
 
     'iconlink':
     {
-        'draw': function (row,field) { 
+        'draw': function (row,field) {
           var icon = 'icon-eye-open'; if (table.fields[field].icon) icon = table.fields[field].icon;
-          return "<a href='"+table.fields[field].link+table.data[row]['id']+"' ><i class='"+icon+"' ></i></a>" 
+          return "<a href='"+table.fields[field].link+table.data[row]['id']+"' ><i class='"+icon+"' ></i></a>"
         }
     },
 
@@ -186,7 +180,7 @@ function list_format_updated(time)
   time = time * 1000;
   var now = (new Date()).getTime();
   var update = (new Date(time)).getTime();
-  var lastupdate = (now-update)/1000;
+  var lastupdate = (now-update)/1000;colu
 
   var secs = (now-update)/1000;
   var mins = secs/60;
@@ -199,12 +193,12 @@ function list_format_updated(time)
 
   var color = "rgb(255,125,20)";
   if (secs<25) color = "rgb(50,200,50)"
-  else if (secs<60) color = "rgb(240,180,20)"; 
+  else if (secs<60) color = "rgb(240,180,20)";
 
   return "<span style='color:"+color+";'>"+updated+"</span>";
 }
 
-// Format value dynamically 
+// Format value dynamically
 function list_format_value(value)
 {
   if (value>=10) value = (1*value).toFixed(1);
