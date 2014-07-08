bebce35c15afbc11993a2f515632f0e1a4c30461
euqip <benoit.pique@gmail.com>
2014-07-08 22:57:49 +0200

    users table does not pass the create process

--- Modules/user/user_schema.php
+++ Modules/user/user_schema.php
@@ -10,7 +10,8 @@ $schema['users'] = array(
     'apikey_write' => array('type' => 'varchar(64)'),
     'apikey_read' => array('type' => 'varchar(64)'),
     'lastlogin' => array('type' => 'datetime'),
-    'admin' => array('type' => 'int(11)', 'Null'=>'NO', 'comments'=>'defines the user role, 1 = system admin'),
+    'admin' => array('type' => 'int(11)', 'Null'=>'NO'),
+    //'admin' => array('type' => 'int(11)', 'Null'=>'NO', 'comments'=>'defines the user role, 1 = system admin'),
 
     // User profile fields
     'gravatar' => array('type' => 'varchar(30)', 'default'=>''),
