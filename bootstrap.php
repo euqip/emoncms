4b958a530793adc7b7b7eb0920cd7e949e1eaeb5
euqip <benoit.pique@gmail.com>
2015-03-23 19:29:36 +0100

    provide default parameters

--- bootstrap.php
+++ bootstrap.php
@@ -33,7 +33,62 @@ define('REGEX_CURRENCY','/[^\w\s£$€¥]/');
 define('REGEX_UNITS','/[^\w\s-°]/');
 define('REGEX_ALPHA_NUM','/[^.\/A-Za-z0-9-=_]/');
 define('REGEX_NUMERIC','/[^.\/0-9,;.=_]/');
+
 //define('REGEX_ALPHA_NUM_ACCENT','/[^\p{L}.\/A-Za-z0-9-=_]/');
+
+//set default parameters, to avoid missing ones
+//these parameters will be overwritten with the settings.php file content
+//
+    $behavior=array(
+        'multiorg'=>true,
+        'min_usernamelen'=>3,
+        'max_usernamelen'=>30,
+        'min_orgnamelen'=>3,
+        'max_orgnamelen'=>10,
+        'min_pwdlen'=>3,
+        'max_pwdlen'=>30,
+
+        'userlist_expanded'=>FALSE,
+        'usergoup'=> "letter",
+        //usergroup may be '', than no groups are made, it is used in users list view
+        'userletter'=> "UCASE(LEFT(username,1)) as letter",
+        //'userletter'=> "1 as letter",
+        //userletter MUST be present but may be a constant, the letter field is used in ORDER BY directive
+        //'userletter'=> "UCASE(LEFT(email,1)) as letter",
+        // grouping by email first letter is an other option
+        'orglist_expanded'=>FALSE,
+        'orggroup'=> "letter",
+        //orggroup may be '', than no groups are made, it is used in organisations list view
+        'orgletter'=> "ucase(LEFT(orgname,1)) as letter",
+        //orgletter MUST be present but may be a constant
+        // see userletter  above and apply same rules, on countries for example
+        //'orgletter'=> "country as letter" will group organisations by countries
+        'inputgroup'=> "nodeid",
+        'inputinterval'=> 5000,
+        'inputlistexpanded'=>TRUE,
+
+        'feedgroup'=> "tag",
+        'feedinterval'=> 5000,
+        'feedlistexpanded'=>1,
+
+        'dashlist_expanded'=>true,
+        'dashgroup'=> "letter",
+        //orggroup may be '', than no groups are made, it is used in organisations list view
+        'dashletter'=> "ucase(LEFT(name,1)) as letter",
+
+        'csv_parameters'=> array(
+          'csvdownloadlimit_mb' => 10,
+          'csv_field_separator'=>";",
+          'csv_decimal_place_separator'=>",",
+          'csv_thousandsepar_separator'=>"",
+          'csv_dateformat'=>"Y-m-d",
+          'csv_timeformat'=>"H:i:s"
+          ),
+
+        );
+
+
+
 /**
  * Load up required libs
  */
