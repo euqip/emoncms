acf01766d33d6885d2c8430dd8b9e2b6b3bbb1f2
euqip <benoit.pique@gmail.com>
2015-04-28 20:10:33 +0200

    create a grid div with css dot matrix, to reduce code execution time

--- Modules/dashboard/Views/dashboard_edit_view.php
+++ Modules/dashboard/Views/dashboard_edit_view.php
@@ -45,8 +45,11 @@ if (!$dashboard['height']) $dashboard['height'] = 400;
 
 <div id="page-container" style="height:<?php echo $dashboard['height']; ?>px; position:relative;">
   <div id="page"><?php echo $dashboard['content']; ?></div>
-  <canvas id="can-old" class="context" data-toggle="context" data-target="#contextmenu" width="940px" height="<?php echo $dashboard['height']; ?>px" style="z-index:200; position:absolute; top:0px; left:0px; margin:0; padding:0;"></canvas>
-  <div id="can" class="context dotted-10" data-toggle="context" data-target="#contextmenu" width="940px" height="<?php echo $dashboard['height']; ?>px" style="z-index:201; position:absolute; top:0px; left:0px; margin:0; padding:0;"></div>
+  <!--
+  <canvas id="canvas" class="context" data-toggle="context" data-target="#contextmenu" width="940px" height="<?php echo $dashboard['height']; ?>px" style="z-index:200; position:absolute; top:0px; left:0px; margin:0; padding:0;"></canvas>
+  -->
+  <div id="can" class="dotted-10" style = "z-index:200; border:none; position: absolute; margin: 0px; top: 0px; left: 0px; width: 1000px; height: 600px;"></div>
+  <div id="ghost" class="resizable draggable " style="z-index: 201; position: absolute; margin: 0px; top: 0px; left: 0px; width: 0px; height: 20px;"></div>
 </div>
 
 <div class="modal fade emoncms-dialog type-primary" id="widget_options" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
